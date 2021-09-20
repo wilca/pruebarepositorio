00c3aabb35a299a82d4a5c98ff577dc1f338695f
wilmar campo <78506146+wilca@users.noreply.github.com>
2021-09-20 16:50:32 -0500

    commit para confg git en otro pc

--- app/src/main/java/co/sfap/pasarapp/MainActivity.java
+++ app/src/main/java/co/sfap/pasarapp/MainActivity.java
@@ -11,6 +11,6 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        Toast.makeText(this, "Hola", Toast.LENGTH_SHORT).show();
+        Toast.makeText(this, "Hola pruebas de repo", Toast.LENGTH_SHORT).show();
     }
 }
